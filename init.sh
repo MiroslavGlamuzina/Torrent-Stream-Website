8860e2a2b9440595fc2819773b3fc7c2ae653433
vibhanshu03 <57120579+vibhanshu03@users.noreply.github.com>
2020-10-02 00:58:34 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
 #!bin/sh
-echo "Installing required packages..."
+echo "Installing Required Packages..."
 apt-get install nodejs -y
 echo "nodejs completed..."
 apt-get install nodejs-legacy -y
 echo "nodejs-legacy completed..."
 apt-get install lamp-server^ -y
-echo "lamp-server completed..."
+echo "Lamp-server Completed..."
 cp -a .data/. /var/www/html/Torrent-Stream-Website/
 sudo chmod a+x .other/Torrent-Stream-Website.desktop
 cp -a .other/Torrent-Stream-Website.desktop ./Torrent-Stream-Website.desktop
 #cp -a .Torrent-Stream-Website.sh Torrent-Stream-Website.sh
-echo "setup completed!"
+echo "Setup Completed!"
 echo "Application Created"
 
 
